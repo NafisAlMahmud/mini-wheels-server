ea915f803a7397b653fd0a6757999e6e48d8d935
Project_3 <anik2115@gmail.com>
2023-05-21 23:41:33 +0600

    server Fixed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -62,6 +62,7 @@ async function run() {
         .find({
           category: req.params.text,
         })
+        .limit(20)
         .toArray();
       res.send(result);
     });
